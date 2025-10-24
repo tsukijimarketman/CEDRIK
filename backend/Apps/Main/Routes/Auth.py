d26ec631efcdb28acf9d7b6189d605321d03cef0
Mark Anthony Asuncion <maasuncion@sscgi.com>
2025-10-24 11:14:14 +0800

    hotfix

--- backend/Apps/Main/Routes/Auth.py
+++ backend/Apps/Main/Routes/Auth.py
@@ -269,7 +269,6 @@ def register():
                     type=AuditType.REGISTER,
                     collection=Collections.USER,
                     id=res_insert,
-                    ip=request.remote_addr,
                     from_data=None,
                     to_data=None
                 )
@@ -377,7 +376,7 @@ def update_user(user_id: str):
                 session=session,
             )
 
-            audit = Audit.audit_collection(
+            audit = audit_collection(
                 type=AuditType.EDIT,
                 collection=Collections.USER,
                 id=target_id,
