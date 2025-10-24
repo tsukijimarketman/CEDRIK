d5fbee46b3f737d8dc99f4388f46cd8a3f556e80
cerabang <cerabang@sscgi.com>
2025-10-24 11:19:21 +0800

    OTP done

--- backend/Apps/Main/Database/Models/__init__.py
+++ backend/Apps/Main/Database/Models/__init__.py
@@ -3,6 +3,7 @@ from .Conversation import Conversation
 from .Message import Message
 from .Memory import *
 from .Audit import Audit
+from .Otp import Otp
 
 def init_indexes():
   User.ensure_indexes()
@@ -10,3 +11,5 @@ def init_indexes():
   Message.ensure_indexes()
   Memory.ensure_indexes()
   Audit.ensure_indexes()
+  Otp.ensure_indexes()
+  
